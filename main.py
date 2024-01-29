dd1d78239e3aa133f06285de57a2f037fa60c78e
Stephen Nunez <runningtechy@gmail.com>
2024-01-29 00:31:20 -0500

    refactor code to use snake case

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ redis_key_toggl_to_google = 'toggl_to_google'
 SCOPES = ['https://www.googleapis.com/auth/calendar', 'https://www.googleapis.com/auth/calendar.events']
 SERVICE_ACCOUNT_FILE = 'google_calendar_service_account_file.json'
 credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-googleService = googleapiclient.discovery.build('calendar', 'v3', credentials=credentials)
+google_service = googleapiclient.discovery.build('calendar', 'v3', credentials=credentials)
 
 def get_current_timestamp():
     return int(datetime.timestamp(datetime.now()))
@@ -65,24 +65,24 @@ def insert_google_calendar_record(summary, description, startDateTime, endDateTi
     }
     }
 
-    event = googleService.events().insert(calendarId=GOOGLE_CALENDAR_ID, body=event).execute()
+    event = google_service.events().insert(calendarId=GOOGLE_CALENDAR_ID, body=event).execute()
     print(f"Inserted Google Calendar event {event['id']}")
     return event
     
-def updateGoogleCalendarRecord(eventId, summary, description, startDateTime, endDateTime):
+def update_google_calendar_record(eventId, summary, description, startDateTime, endDateTime):
     print(f"Updating Google Calendar event {eventId}: summary={summary}, description={description}, startDateTime={startDateTime}, endDateTime={endDateTime}")
 
-    event = googleService.events().get(calendarId=GOOGLE_CALENDAR_ID, eventId=eventId).execute()
+    event = google_service.events().get(calendarId=GOOGLE_CALENDAR_ID, eventId=eventId).execute()
     
     event['summary'] = summary
     event['description'] = description
     event['start']['dateTime'] = startDateTime
     event['end']['dateTime'] = endDateTime
 
-    updated_event = googleService.events().update(calendarId=GOOGLE_CALENDAR_ID, eventId=event['id'], body=event).execute()
+    updated_event = google_service.events().update(calendarId=GOOGLE_CALENDAR_ID, eventId=event['id'], body=event).execute()
     return updated_event
 
-def getGoogleIdSyncRecord(togglId):
+def get_google_id_sync_record(togglId):
     key = f'{redis_key_toggl_to_google}:{togglId}'
     google_calendar_event_id = redis_object.get(f'{redis_key_toggl_to_google}:{togglId}')
 
@@ -92,43 +92,46 @@ def getGoogleIdSyncRecord(togglId):
         print(f'Google sync record does not exist for key: {key}')
         return ""
 
-def insertSyncRecord(togglId, googleId):
+def insert_sync_record(togglId, googleId):
     redis_object.set(f'{redis_key_toggl_to_google}:{togglId}', googleId)
         
-def updateLastSyncTime():
+def update_last_sync_time():
     redis_object.set(redis_key_toggl_last_sync_time, get_current_timestamp())
             
-def deleteGoogleEvent(googleEventId):
+def delete_google_event(googleEventId):
     print(f"Deleting Google Calendar event {googleEventId}")
-    googleService.events().delete(calendarId=GOOGLE_CALENDAR_ID, eventId=googleEventId).execute()
+    google_service.events().delete(calendarId=GOOGLE_CALENDAR_ID, eventId=googleEventId).execute()
     
-def deleteSyncRecordWithGoogleId(togglId):
+def delete_sync_record_with_google_id(togglId):
     redis_object.delete(f'{redis_key_toggl_to_google}:{togglId}')
+    
+def sync_toggl_to_google_calendar():
+    data = requests.get(f'https://api.track.toggl.com/api/v9/me/time_entries?since={get_last_sync_time()}&meta=true', headers={'content-type': 'application/json', 'Authorization' : f'Basic {b64encode(bytes(f'{TOGGL_USER_NAME}:{TOGGL_PASSWORD}', 'utf-8')).decode('ascii')}'})
+    for record in data.json():
+        togglId = record.get("id", "")
+        clientName = record.get("client_name", "")
+        projectName = record.get("project_name", "")
+        description = record.get("description", "")
+        startTime = record.get("start", "")
+        endTime = record.get("stop", "")
+        serverDeletedAt = record.get("server_deleted_at", "")
+        
+        if(len(description) > 0):
+            summary = description + '-' + projectName
+        else:
+            summary = projectName
+        
+        googleEventId = get_google_id_sync_record(togglId)
+        if (len(googleEventId) > 0 and serverDeletedAt is not None and len(serverDeletedAt) > 0):
+            delete_google_event(googleEventId)
+            delete_sync_record_with_google_id(googleEventId)
+        elif (len(googleEventId) > 0):
+            update_google_calendar_record(googleEventId, summary, clientName, startTime, endTime)
+        else:
+            insertedGoogleEvent = insert_google_calendar_record(summary, clientName, startTime, endTime)
+            insert_sync_record(togglId, insertedGoogleEvent['id'])
+    update_last_sync_time()
 
 
 #####################################################################
-data = requests.get(f'https://api.track.toggl.com/api/v9/me/time_entries?since={get_last_sync_time()}&meta=true', headers={'content-type': 'application/json', 'Authorization' : f'Basic {b64encode(bytes(f'{TOGGL_USER_NAME}:{TOGGL_PASSWORD}', 'utf-8')).decode('ascii')}'})
-for record in data.json():
-    togglId = record.get("id", "")
-    clientName = record.get("client_name", "")
-    projectName = record.get("project_name", "")
-    description = record.get("description", "")
-    startTime = record.get("start", "")
-    endTime = record.get("stop", "")
-    serverDeletedAt = record.get("server_deleted_at", "")
-    
-    if(len(description) > 0):
-        summary = description + '-' + projectName
-    else:
-        summary = projectName
-    
-    googleEventId = getGoogleIdSyncRecord(togglId)
-    if (len(googleEventId) > 0 and serverDeletedAt is not None and len(serverDeletedAt) > 0):
-        deleteGoogleEvent(googleEventId)
-        deleteSyncRecordWithGoogleId(googleEventId)
-    elif (len(googleEventId) > 0):
-        updateGoogleCalendarRecord(googleEventId, summary, clientName, startTime, endTime)
-    else:
-        insertedGoogleEvent = insert_google_calendar_record(summary, clientName, startTime, endTime)
-        insertSyncRecord(togglId, insertedGoogleEvent['id'])
-updateLastSyncTime()
+sync_toggl_to_google_calendar()
